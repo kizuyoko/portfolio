135c5c6efef37b4c66d8432338bcd5eb5302483a
kizuyoko <kizuyoko@gmail.com>
2024-10-01 16:36:19 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <section className="flex items-center justify-center h-screen bg-violet-900 text-white">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">Welcome to my Portfolio!</h1>
+          <p className="mt-2 text-lg">This is an example text in the middle of the screen.</p>
+        </div>
+      </section>
+      <section className="flex items-center justify-center pt-20 pb-20">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">Example Title</h1>
+          <p className="mt-2 text-lg">This is an example text in the middle of the screen.</p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
