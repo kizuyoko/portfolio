e8216d596084bc3b4721ebd7425add1e26d25f5a
kizuyoko <kizuyoko@gmail.com>
2024-10-08 17:54:12 +0200

    Debug in ProjectListTitle

--- src/componenmts/ProjectListTitle.tsx
+++ src/componenmts/ProjectListTitle.tsx
@@ -1,10 +1,12 @@
 const ProjectListTitle: React.FC = () => {
   return (
-    <section className="container p-4 pt-10 pb-10 w-full flex flex-col items-center justify-center">
-      <h2 className="text-3xl font-bold text-center text-gray-700">My Projects</h2>
-      <p className="mt-4 text-lg text-center">
-        Explore a collection of my latest projects that showcase my skills and creativity. 
-      </p>
+    <section className="p-4 pt-10 pb-10 w-full flex flex-col items-center justify-center">
+      <div className="max-w-4xl">
+        <h2 className="text-3xl font-bold text-center text-gray-700">My Projects</h2>
+        <p className="mt-4 text-lg text-center">
+          Explore a collection of my latest projects that showcase my skills and creativity. 
+        </p>
+      </div>
     </section>
   );
 };
