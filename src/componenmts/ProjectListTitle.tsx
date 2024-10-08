85c4eaf99d4117ec7bacba4e5178f5abf7557d05
kizuyoko <kizuyoko@gmail.com>
2024-10-08 17:37:21 +0200

    Small styles

--- src/componenmts/ProjectListTitle.tsx
+++ src/componenmts/ProjectListTitle.tsx
@@ -1,8 +1,8 @@
 const ProjectListTitle: React.FC = () => {
   return (
-    <section className="p-4 pt-10 pb-10 w-full flex flex-col items-center justify-center">
+    <section className="container p-4 pt-10 pb-10 w-full flex flex-col items-center justify-center">
       <h2 className="text-3xl font-bold text-center text-gray-700">My Projects</h2>
-      <p className="m-4 text-lg text-center">
+      <p className="mt-4 text-lg text-center">
         Explore a collection of my latest projects that showcase my skills and creativity. 
       </p>
     </section>
