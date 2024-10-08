240a075f0d8ddc97d1db1803f5454e59f653c02b
kizuyoko <kizuyoko@gmail.com>
2024-10-08 17:18:08 +0200

    Small color change

--- src/componenmts/ProjectListTitle.tsx
+++ src/componenmts/ProjectListTitle.tsx
@@ -1,7 +1,7 @@
 const ProjectListTitle: React.FC = () => {
   return (
     <section className="p-4 pt-10 pb-10 w-full flex flex-col items-center justify-center">
-      <h2 className="text-3xl font-bold text-center text-gray-800">My Projects</h2>
+      <h2 className="text-3xl font-bold text-center text-gray-700">My Projects</h2>
       <p className="m-4 text-lg text-center">
         Explore a collection of my latest projects that showcase my skills and creativity. 
       </p>
