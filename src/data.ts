3e5346568bf9c0f3fc9b58b9cd003d76f1a94ef1
kizuyoko <kizuyoko@gmail.com>
2024-10-09 15:15:47 +0200

    Removed some projects

--- src/data.ts
+++ src/data.ts
@@ -26,15 +26,17 @@ const projects: ProjectType[] = [
     stack: ['Tailwind', 'JavaScript', 'HTML', 'CSS'],
     demo: 'https://tailwind-showcase-2l1f.vercel.app',
     git: 'https://github.com/kizuyoko/tailwind-showcase',
-  }, {
-    id: 'typeScriptReduxClothing',
-    title: 'TypeScript Redux Clothing',
-    image: 'typescript_redux_cloth.png',
-    description: 'Cloth Shopping. List of products, cart.',
-    stack: ['React.js', 'TypeScript', 'Redux', 'Vite'],
-    demo: 'https://typescript-redux-cloth.vercel.app',
-    git: 'https://github.com/kizuyoko/typescript-redux-cloth',
-  }, {
+  },{
+    id: 'reactNativeColor',
+    title: 'React Native Color',
+    image: 'react_native_color.png',
+    description: "Colors List, today's color (random), form to add a new color.",
+    stack: ['React Native', 'Expo', 'Android', 'iOS', 'EAS'],
+    demo: 'https://expo.dev/accounts/kizuyoko/projects/react-native-start/builds/d1518ce4-4d92-47b2-9aa3-a207f5271b56',
+    git: 'https://github.com/kizuyoko/react-native-color',
+  },  
+/*
+  {
     id: 'nextJsFood',
     title: 'Next.js Food',
     image: 'nextjs_food.png',
@@ -51,14 +53,6 @@ const projects: ProjectType[] = [
     demo: 'https://react-patterns-travel.vercel.app',
     git: 'https://github.com/kizuyoko/react-patterns-travel',
   }, {
-    id: 'reactNativeColor',
-    title: 'React Native Color',
-    image: 'react_native_color.png',
-    description: "Colors List, today's color (random), form to add a new color.",
-    stack: ['React Native', 'Expo', 'Android', 'iOS', 'EAS'],
-    demo: 'https://expo.dev/accounts/kizuyoko/projects/react-native-start/builds/d1518ce4-4d92-47b2-9aa3-a207f5271b56',
-    git: 'https://github.com/kizuyoko/react-native-color',
-  }, {
     id: 'reactNativeFood',
     title: 'React Native Food',
     image: 'react_native_food.png',
@@ -66,7 +60,16 @@ const projects: ProjectType[] = [
     stack: ['React Native', 'Redux', 'EAS', 'Expo', 'Android', 'iOS' ],
     demo: 'https://expo.dev/accounts/kizuyoko/projects/react-native-food/builds/db2c58cb-05dc-4409-aac2-4a7659182381',
     git: 'https://github.com/kizuyoko/react-native-food',
-  },
+  },{
+    id: 'typeScriptReduxClothing',
+    title: 'TypeScript Redux Clothing',
+    image: 'typescript_redux_cloth.png',
+    description: 'Cloth Shopping. List of products, cart.',
+    stack: ['React.js', 'TypeScript', 'Redux', 'Vite'],
+    demo: 'https://typescript-redux-cloth.vercel.app',
+    git: 'https://github.com/kizuyoko/typescript-redux-cloth',
+  }, 
+*/  
 ]
 
 export default projects;
