64c60cc59b2d64b77aef46b048a5f74b0edea2a8
kizuyoko <kizuyoko@gmail.com>
2025-08-07 18:02:16 +0200

    Mention about new portfolio

--- src/componenmts/Header.tsx
+++ src/componenmts/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <section className="flex flex-col items-center justify-center h-screen bg-custom-image bg-cover bg-center text-center text-white p-4">
+    <section className="flex flex-col items-center justify-center h-screen p-4 text-center text-white bg-center bg-cover bg-custom-image">
       <div className="flex flex-col items-center justify-center max-w-4xl animate-fade-up animate-ease-out animate-duration-[3500ms]">
         <h1>Welcome to Yoko's Portfolio!</h1>
         <p>Scroll down to explore the features. </p>
@@ -13,6 +13,11 @@ const Header: React.FC = () => {
         </p>
         <p></p>
         <small className="text-slate-300">Some of them might be expired, please contact me to activate if you want to try.</small>
+        <p className="p-4 mt-6 border border-white rounded">
+          This portfolio is currently being rebuilt!<br />
+          A new and improved version is on its way.<br />
+          Preview it <a href="https://yoko-portfolio-kappa.vercel.app" target="_blank">here</a>.
+        </p>
       </div>
     </section>
   );
